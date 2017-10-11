bde3669bef65d83afced7d87207edbda673b4380
Victor Weidar <Victor.weidar@gmail.com>
2017-10-11 17:51:00 +1000

    + NEW IDEA +Movement Complete

---
+++ OfficeChairGame/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float direction;
+    public float _speed = 10f;
+    public Transform target;
+    Transform me;
+
+	// Use this for initialization
+	void Start ()
+    {
+        me = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float Speed = direction * _speed * Time.deltaTime;
+        me.position = Vector3.MoveTowards(this.transform.position, target.position, Speed);
+	}
+}
